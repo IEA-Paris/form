6beba92906db84fa8ed3e74849c99ffb21be35b8
Hamed Bouare <hamed.bouare@paris-iea.fr>
2025-08-12 00:43:52 +0200

    fix minor bugs

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxtjs/i18n",
+    "@nuxtjs/apollo",
+    "@pinia/nuxt",
+    "@nuxtjs/mdc",
+  ],
+  imports: {
+    autoImport: false,
+  },
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  build: {
+    transpile: ["vuetify", "@paris-ias/list"],
+  },
+  pinia: {
+    autoImports: ["defineStore", ["defineStore", "definePiniaStore"]],
+  },
+})
